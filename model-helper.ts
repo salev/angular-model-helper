693b6b95fcb2b0c97318342b7206505d70d9d256
Alexander Levakov <salev127@gmail.com>
2018-11-07 12:17:39 +0200

    Update model-helper.ts

--- model-helper.ts
+++ model-helper.ts
@@ -1,4 +1,8 @@
-import {enumerable} from "./_decorators";
+function enumerable(value: boolean) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    descriptor.enumerable = value;
+  };
+}
 
 const DEFAULT_DATE_FORMAT: string = 'YYYYMMDD';
 
